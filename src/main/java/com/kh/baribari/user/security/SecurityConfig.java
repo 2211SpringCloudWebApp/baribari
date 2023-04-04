8ba3a7964f25ca62c500cdcc2d41cf3fffdc552c
서정민 <ywcsky2@gmail.com>
2023-04-04 16:50:32 +0900

    add : Spring Security 로그인창 안나오게 수정함

---
+++ src/main/java/com/kh/baribari/user/security/SecurityConfig.java
@@ -0,0 +1,7 @@
+package com.kh.baribari.user.security;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+}

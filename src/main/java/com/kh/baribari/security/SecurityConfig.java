cd996d38c1f03fd7f72c3db17c5aed4ae628c696
서정민 <ywcsky2@gmail.com>
2023-04-14 09:28:24 +0900

    2023-04-14 init : 스프링시큐리티 에러 수정

--- src/main/java/com/kh/baribari/security/SecurityConfig.java
+++ src/main/java/com/kh/baribari/security/SecurityConfig.java
@@ -1,10 +1,12 @@
-package com.kh.baribari.user.security;
+package com.kh.baribari.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,19 +22,19 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-//                .antMatchers("/").permitAll()
-//                .antMatchers("/login").permitAll()
-                .anyRequest().permitAll()
+                .antMatchers("/", "/login").permitAll()
+                .antMatchers("/register*").permitAll()
+                .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login")
                 .loginProcessingUrl("/loginProc")
                 .usernameParameter("userId")
                 .passwordParameter("userPw")
-                .defaultSuccessUrl("/",true)
+                .defaultSuccessUrl("/", true)
                 .permitAll()
                 .and()
                 .logout()
@@ -42,8 +44,11 @@ public class SecurityConfig {
         return http.build();
     }
 
+
+
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer(){
         return (web) -> web.ignoring().antMatchers("/static/js/**","/static/css/**", "/static/img/**", "/static/**" , "/template/**");
     }
+
 }

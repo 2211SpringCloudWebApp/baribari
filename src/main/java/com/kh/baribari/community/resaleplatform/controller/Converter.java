eea5de5342b5a18c4c6ab085f76d43cab9ccb95a
gagagaga <yoongagagaga@gmail.com>
2023-05-01 09:18:07 +0900

    2023-05-1 Add : HashtagService, Converter

---
+++ src/main/java/com/kh/baribari/community/resaleplatform/controller/Converter.java
@@ -0,0 +1,67 @@
+package com.kh.baribari.community.resaleplatform.controller;
+
+import com.kh.baribari.community.resaleplatform.domain.dto.ArticleHashtagDto;
+import com.kh.baribari.community.resaleplatform.domain.dto.GetArticleDto;
+import com.kh.baribari.community.resaleplatform.domain.dto.ArticleCommentResponse;
+import com.kh.baribari.community.resaleplatform.domain.dto.GetArticleResponse;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Converter
+{
+    public static GetArticleResponse toGetArticleResponse(GetArticleDto dto) {
+        Set<ArticleCommentResponse> articleCommentsResponse = dto.getArticleCommentDto().stream()
+                .map(commentDto -> ArticleCommentResponse.builder()
+                        .commentNo(commentDto.getCommentNo())
+                        .commentContent(commentDto.getContent())
+                        .userNo(commentDto.getUserDto().getUserId())
+                        .userNickname(commentDto.getUserDto().getUserNickName())
+                        .parentCommentNo(commentDto.getParentCommentNo() == null ? null : commentDto.getParentCommentNo())
+                        .build())
+                .collect(Collectors.toSet());
+
+        return GetArticleResponse.builder()
+                .communityNo(dto.getCommunityNo())
+                .communitySubject(dto.getCommunitySubject())
+                .communityContent(dto.getCommunityContent())
+                .hashtags(dto.getArticleHashtagDtos().stream()
+                        .map(ArticleHashtagDto::getHashtagName)
+                        .collect(Collectors.toSet()))
+                .communityDate(dto.getCommunityDate())
+                .nickname(dto.getUserDto().getUserNickName())
+                .userId(dto.getUserDto().getUserId())
+                .articleCommentsResponse(articleCommentsResponse)
+                .build();
+    }
+
+//    public static ArticleWithCommentsDto toArticleWithCommentsDto(ArticleWithCommentResponse response) {
+//        Set<ArticleCommentDto> articleCommentDto = response.getArticleCommentsResponse().stream()
+//                .map(commentResponse -> ArticleCommentDto.builder()
+//                        .id(commentResponse.getCommentNo())
+//                        .content(commentResponse.getCommentContent())
+//                        .userDto(UserDto.builder()
+//                                .userId(commentResponse.getUserNo())
+//                                .userNickName(commentResponse.getUser().getUserNickName())
+//                                .build())
+//                        .parentCommentNo(commentResponse.getParentCommentNo() == null ? null : ArticleCommentDto.builder().id(commentResponse.getParentCommentNo())
+//                                        .build())
+//                        .build())
+//                .collect(Collectors.toSet());
+//
+//        return ArticleWithCommentsDto.builder()
+//                .communityNo(response.getCommunityNo())
+//                .userDto(UserDto.builder()
+//                        .userId(response.getUserId())
+//                        .userNickName(response.getNickname())
+//                        .build())
+//                .articleCommentDto(articleCommentDto)
+//                .communitySubject(response.getCommunitySubject())
+//                .communityContent(response.getCommunityContent())
+//                .articleHashtagDtos(response.getHashtags().stream()
+//                        .map(hashtag -> ArticleHashtagDto.builder().hashtagName(hashtag).build())
+//                        .collect(Collectors.toSet()))
+//                .communityDate(response.getCommunityDate())
+//                .build();
+//    }
+}
---
+++ src/main/java/com/kh/baribari/community/resaleplatform/service/HashtagService.java
@@ -0,0 +1,50 @@
+package com.kh.baribari.community.resaleplatform.service;
+
+import com.kh.baribari.community.resaleplatform.domain.ArticleHashtag;
+import com.kh.baribari.community.resaleplatform.repository.ArticleHashtagRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+@Service
+@RequiredArgsConstructor
+public class HashtagService {
+
+
+
+    private final ArticleHashtagRepository hashtagRepository;
+
+    public Set<ArticleHashtag> findHashtagsByNames(Set<String> hashtagNames) {
+        return new HashSet<>(hashtagRepository.findByHashtagNameIn(hashtagNames));
+    }
+
+    public Set<String> parseHashtagNames(String content) {
+        if (content == null) {
+            return new HashSet<>();
+        }
+        Pattern pattern = Pattern.compile("#[\\w가-힣]+");
+        Matcher matcher = pattern.matcher(content.strip());
+        Set<String> result = new HashSet<>();
+
+        while (matcher.find()) {
+            result.add(matcher.group().replace("#", ""));
+        }
+
+        return Set.copyOf(result);
+    }
+
+    public void deleteHashtagWithoutArticles(Integer communityNo) {
+        Optional<ArticleHashtag> hashtag = hashtagRepository.findById(communityNo);
+        if (hashtag.get().getArticles().isEmpty()) {
+            hashtagRepository.delete(hashtag.get().getCommunityNo());
+        }
+    }
+
+
+}
